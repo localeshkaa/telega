933b105f702ae5707766addbcbb7d3ae9e363dc5
Gleb <57525153+w0dge@users.noreply.github.com>
2021-12-22 15:02:54 +0300

    Update test.py

--- tests/test.py
+++ tests/test.py
@@ -5,6 +5,10 @@ class Testing(unittest.TestCase):
     def test_string(self):
         a = 'some'
         b = 'some'
+        if self.assertEqual(a, b):
+            print("Yeah! It's all good 1")
+        else:
+            print("You died 1")
         return self.assertEqual(a, b)
 
 
@@ -12,6 +16,10 @@ class Testing(unittest.TestCase):
         a = True
         b = True
         self.assertEqual(a, b)
+        if self.assertEqual(a, b):
+            print("Yeah! It's all good 2")
+        else:
+            print("You died 2")
         return self.assertEqual(a, b)
 
 if __name__ == '__main__':
