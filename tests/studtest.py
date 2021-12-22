ef9cf406e6ce234da688b9eecc1bc725960d3f0c
Gleb <57525153+w0dge@users.noreply.github.com>
2021-12-22 14:53:18 +0300

    Update studtest.py

--- tests/studtest.py
+++ tests/studtest.py
@@ -5,11 +5,11 @@ from Bot import token
 bot = telebot.TeleBot(token)
 
 if Testing.test_string():
-    return bot.send_message(message.chat.id, "Yuppy, info is correct")
+     bot.send_message(message.chat.id, "Yuppy, info is correct")
 else:
-    return bot.send_message(message.chat.id, ":(), info is not correct")
+     bot.send_message(message.chat.id, ":(), info is not correct")
 
 if Testing.test_boolean():
-    return bot.send_message(message.chat.id, "Yuppy, info 2  is correct")
+     bot.send_message(message.chat.id, "Yuppy, info 2  is correct")
 else:
-    return bot.send_message(message.chat.id, ":(), info 2 is not correct")
+     bot.send_message(message.chat.id, ":(), info 2 is not correct")
